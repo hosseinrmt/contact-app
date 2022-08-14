b2ca21c6fa5a67e205bc8209642674e396d5308e
hossein Rahmati <rmthossein@gmail.com>
2022-08-14 18:42:52 +0430

    Fix some bugs - store contacts in localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import ContactApp from "./components/ContactApp";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (formValues) => {
+    if (formValues.name && formValues.email) {
+      setContacts([...contacts, { ...formValues, id: Date.now() }]);
+      return;
+    }
+    toast.error("Name and email are required");
+  };
+
+  const deleteContactHandler = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  useEffect(() => {
+    const contacts = JSON.parse(localStorage.getItem("contacts"));
+    if (contacts) {
+      setContacts(contacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <>
-      <ToastContainer />
-      <ContactApp />
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        draggable
+      />
+
+      <div className="contact-app">
+        <nav>
+          <h1>Contact Manager</h1>
+        </nav>
+        <ContactForm onClick={addContactHandler} />
+        <ContactList onDelete={deleteContactHandler} contacts={contacts} />
+      </div>
     </>
   );
 };
