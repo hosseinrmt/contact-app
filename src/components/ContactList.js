a6abfda6e2ad051bcf37e4158121627a7ff28ff9
hossein Rahmati <rmthossein@gmail.com>
2022-08-18 00:29:31 +0430

    deploy to netlify

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,10 +5,15 @@ import { useEffect, useState } from "react";
 import deleteContact from "../service/deleteContactService";
 
 const ContactList = () => {
+  const [allcontacts, setAllContacts] = useState([]);
   const [contacts, setContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    getContacts().then((res) => setContacts(res.data));
+    getContacts().then((res) => {
+      setContacts(res.data);
+      setAllContacts(res.data);
+    });
   }, []);
 
   const deleteContactHandler = (id) => {
@@ -16,13 +21,42 @@ const ContactList = () => {
     setContacts(contacts.filter((contact) => contact.id !== id));
   };
 
+  const changeHandler = (e) => {
+    setSearchTerm(e.target.value);
+    const searchValue = e.target.value.toLowerCase();
+
+    if (searchValue !== "") {
+      const filteredContacts = allcontacts.filter((contact) => {
+        return Object.values(contact)
+          .join(" ")
+          .toLocaleLowerCase()
+          .includes(searchValue);
+      });
+      setContacts(filteredContacts);
+    } else {
+      setContacts(allcontacts);
+    }
+  };
+
   return (
     <div className="contact-list">
-      <h2>Contacts List</h2>
+      <div className="flex flex-col items-center md:flex-row md:justify-between">
+        <h2>Contacts List</h2>
 
-      <Link to="/add-contact">
-        <button className="addNewBtn">New Contact ?</button>
-      </Link>
+        <Link to="/add-contact">
+          <button className="addNewBtn">New Contact ?</button>
+        </Link>
+      </div>
+
+      <div>
+        <input
+          className="flex w-full"
+          value={searchTerm}
+          onChange={changeHandler}
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
 
       {contacts ? (
         contacts.map((contact) => {
